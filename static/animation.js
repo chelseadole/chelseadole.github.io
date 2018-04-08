be494f5af0ca700f7f9086f423a1840077b7d7d6
CHELSEA DOLE <chelseadole@gmail.com>
2018-04-08 03:31:08 -0700

    deleting console log

--- static/animation.js
+++ static/animation.js
@@ -1,36 +1,4 @@
 
-
-var slideIndex = 1;
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
-
-
-
 // All credit to: https://onextrapixel.com/creating-a-swipeable-side-menu-for-the-web/
 $(document).ready(function() {
   $("[data-toggle]").click(function() {
