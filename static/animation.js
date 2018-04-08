c865a4b60b7909929d518dd518f953fefbb137fb
CHELSEA DOLE <chelseadole@gmail.com>
2018-04-08 16:56:13 -0700

    Fixing nav bar error, moving jquery cdn back to top.

--- static/animation.js
+++ static/animation.js
@@ -1,23 +1,4 @@
+$("#sidebar-toggle").click(function() {
+    $(".container").toggleClass("open-sidebar");
 
-// All credit to: https://onextrapixel.com/creating-a-swipeable-side-menu-for-the-web/
-$(document).ready(function() {
-  $("[data-toggle]").click(function() {
-    var toggle_el = $(this).data("toggle");
-    $(toggle_el).toggleClass("open-sidebar");
-  });
-     
-});
- 
-$("#sidebar-toggle").click({
-    swipeStatus:function(event, phase, direction, distance, duration, fingers)
-        {
-            if (phase=="move" && direction =="right") {
-                 $(".container").addClass("open-sidebar");
-                 return false;
-            }
-            if (phase=="move" && direction =="left") {
-                 $(".container").removeClass("open-sidebar");
-                 return false;
-            }
-        }
 });
