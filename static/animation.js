ddd3d9479ebed3f06c6e75d38a75e26d305e6772
CHELSEA DOLE <chelseadole@gmail.com>
2018-04-08 03:18:46 -0700

    fixing contact page.

--- static/animation.js
+++ static/animation.js
@@ -1,4 +1,5 @@
 
+
 var slideIndex = 1;
 var slides = document.getElementsByClassName("mySlides");
 var dots = document.getElementsByClassName("dot");
@@ -16,8 +17,6 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  // var slides = document.getElementsByClassName("mySlides");
-  // var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
